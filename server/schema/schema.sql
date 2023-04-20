fe46a433799c7181cf82b5331ed94777e0d9eb5e
zirris512 <zirris512@gmail.com>
2023-04-20 10:54:45 -0500

    add database schema

---
+++ server/schema/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS login;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email text UNIQUE NOT NULL,
+    entries BIGINT DEFAULT 0,
+    joined TIMESTAMP NOT NULL
+);
+CREATE TABLE login (
+    id SERIAL PRIMARY KEY,
+    hash VARCHAR(100) NOT NULL,
+    email text UNIQUE NOT NULL
+);
