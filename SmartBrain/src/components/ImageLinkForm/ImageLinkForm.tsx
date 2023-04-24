361255c977498ead440aa32aa1866dbdced86383
zirris512 <zirris512@gmail.com>
2023-04-23 19:40:01 -0500

    add placeholder text

--- SmartBrain/src/components/ImageLinkForm/ImageLinkForm.tsx
+++ SmartBrain/src/components/ImageLinkForm/ImageLinkForm.tsx
@@ -11,7 +11,12 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }: ImageLinkFormPropType)
 			<p className="f3">This Magic Brain will detect faces in your pictures. Give it a try.</p>
 			<div className="flex justify-center">
 				<div className="form flex justify-center pa4 br3 shadow-5">
-					<input type="text" className="f4 pa2 w-70 center" onChange={onInputChange} />
+					<input
+						type="text"
+						className="f4 pa2 w-70 center"
+						onChange={onInputChange}
+						placeholder="Enter an image url..."
+					/>
 					<button
 						className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
 						onClick={onButtonSubmit}
